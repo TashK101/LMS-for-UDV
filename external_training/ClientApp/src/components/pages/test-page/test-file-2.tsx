05429f146720a1941c61aa6016787092394b94a7
Mariya-Lisachenko <paramarabara@mail.ru>
2023-12-27 01:02:20 +0500

    add tailwind

--- external_training/ClientApp/src/components/pages/test-page/test-file-2.tsx
+++ external_training/ClientApp/src/components/pages/test-page/test-file-2.tsx
@@ -4,8 +4,8 @@ type DropDownMenuElementProps = {
 export default function DropDownMenuElement({elementLabel} : DropDownMenuElementProps) : JSX.Element {
     return (
         <div className="self-stretch h-14 flex-col justify-start items-start flex">
-            <div className="self-stretch h-14 px-3 py-2 justify-start items-center gap-3 inline-flex">
-                <div className="w-6 h-6 relative bg-[#FFEDCF]" />
+            <div className="self-stretch h-14 px-3 py-2 justify-start items-center gap-3 inline-flex ">
+                <div className="w-6 h-6 relative " />
                 <div className="grow shrink basis-0 flex-col justify-start items-start inline-flex">
                     <div className="self-stretch text-zinc-900 text-base font-normal font-['Golos']">{elementLabel}</div>
                 </div>
