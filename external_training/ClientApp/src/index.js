40126c0e4fbaacb1ae36bb15bd91bf3a338231a1
Mariya-Lisachenko <paramarabara@mail.ru>
2023-12-28 04:09:53 +0500

    add clsx

--- external_training/ClientApp/src/index.js
+++ external_training/ClientApp/src/index.js
@@ -5,11 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import {} from "./tailwind.css";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-import {} from "./tailwind.css";
 
 root.render(
   <BrowserRouter basename={baseUrl}>
