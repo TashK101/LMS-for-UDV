a548d1811e7ea25a51ebed65013fb648278e14c1
Mariya-Lisachenko <paramarabara@mail.ru>
2024-01-15 14:50:22 +0500

    add menu

--- external_training/ClientApp/src/components/header/header.tsx
+++ external_training/ClientApp/src/components/header/header.tsx
@@ -7,10 +7,13 @@ import {useNavigate} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../hooks";
 import {fetchNotificationsAction, fetchStartConfigAction} from "../../store/api-actions/api-actions";
 import {getfullName, getRole} from "../../store/system-process/system-getters";
+import {DropdownMenu} from "reactstrap";
+import DropDownMenu from "../drop-down-menu/drop-down-menu";
 
 export function Header () : JSX.Element {
     const [isBellHover, setIsBellHover] = useState<boolean>(false);
     const [isLetterHover, setIsLetterHover] = useState<boolean>(false);
+    const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     useEffect(() => {
@@ -22,17 +25,20 @@ export function Header () : JSX.Element {
     const userId = 1;
 
     return (
-        <div className="w-full inline-flex relative bg-white shadow">
-            <div className="w-1/2 px-[30px]">
-                <Logo/>
-            </div>
-            <div className="w-1/2 px-[50px] py-[15px]">
-                <div className="flex justify-end items-center gap-[22px]">
-                    <button onMouseEnter={() => setIsBellHover(true)} onMouseLeave={() => setIsBellHover(false)} onClick={() => navigate('/notifications', {state: userId})}><Bell isHover={isBellHover}/></button>
-                    {role === 'Admin' && <button onMouseEnter={() => setIsLetterHover(true)} onMouseLeave={() => setIsLetterHover(false)} onClick={() => navigate('/catalogapplications')}><Letter isHover={isLetterHover}/></button>}
-                    <HeaderAvatar userFullName={fullName}/>
+        <div>
+            <div className="w-full inline-flex relative bg-white shadow">
+                <div className="w-1/2 px-[30px]">
+                    <Logo/>
+                </div>
+                <div className="w-1/2 px-[50px] py-[15px]">
+                    <div className="flex justify-end items-center gap-[22px]">
+                        <button onMouseEnter={() => setIsBellHover(true)} onMouseLeave={() => setIsBellHover(false)} onClick={() => navigate('/notifications', {state: userId})}><Bell isHover={isBellHover}/></button>
+                        {role === 'Admin' && <button onMouseEnter={() => setIsLetterHover(true)} onMouseLeave={() => setIsLetterHover(false)} onClick={() => navigate('/catalogapplications')}><Letter isHover={isLetterHover}/></button>}
+                        <button onClick={() => setIsMenuVisible(!isMenuVisible)}><HeaderAvatar userFullName={fullName}/></button>
+                    </div>
                 </div>
             </div>
+            {<DropDownMenu isVisible={isMenuVisible}/>}
         </div>
     )
 }
