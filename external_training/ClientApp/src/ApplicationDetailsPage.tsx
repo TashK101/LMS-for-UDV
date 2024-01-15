42e8dd734ba7a18cdc8371ebf32eee55cc5ae46e
TashK101 <Natalia.Karimova@urfu.me>
2024-01-15 14:08:25 +0500

    id route 2

--- external_training/ClientApp/src/ApplicationDetailsPage.tsx
+++ external_training/ClientApp/src/ApplicationDetailsPage.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { RouteProps } from 'react-router';
 import { ApplicationDetails} from "./components/application-details/application-details";
 
