495b15fa5204caade995e5136e05370bc08d7a87
Mariya-Lisachenko <paramarabara@mail.ru>
2024-01-17 23:51:42 +0500

    fix some small bugs

--- external_training/ClientApp/src/components/pages/notifications-page/notifications-page.tsx
+++ external_training/ClientApp/src/components/pages/notifications-page/notifications-page.tsx
@@ -12,12 +12,13 @@ export function NotificationsPage() : JSX.Element {
     }, []);
     const notifications = useAppSelector(getNotifications);
     return(
-        <div className="w-screen h-screen relative bg-white">
+        <div className="w-full h-full relative bg-white">
             <Header/>
             <div className="pl-[350px] pt-[220px] pb-[50px] text-zinc-800 text-[32px] font-semibold font-['Golos'] tracking-wider">Уведомления</div>
             <div className="pl-[355px] flex-col justify-start items-start gap-[40px] inline-flex">
                 {notifications.map((element) => <Notification key={element.createdAt} notification={element}/>)}
             </div>
+            <div className="h-[100px]"/>
         </div>
     )
 }
