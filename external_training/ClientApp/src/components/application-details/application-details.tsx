36cd99696cf9eaef0aaf0a982b57e6d799bb5202
TashK101 <Natalia.Karimova@urfu.me>
2024-06-16 15:40:12 +0500

    some merging + some application stuff

--- external_training/ClientApp/src/components/application-details/application-details.tsx
+++ external_training/ClientApp/src/components/application-details/application-details.tsx
@@ -19,6 +19,8 @@ import {AcceptDeclineButton} from "./accept-decline-buttons";
 import {SubmitButton4} from "../common/Button";
 import {StatusIcon} from "../current-applications-utils/icons/status-icons.tsx";
 import {ApplicationStatus} from "../current-applications-utils/application-status.ts";
+import PendingApplicationDetails from "./pending-application-details";
+import ApprovedApplicationDetails from "./approved-application-details";
 
 export type ApplicationDetailsProps = {
     id: number;
@@ -45,67 +47,30 @@ export function ApplicationDetails({id}: ApplicationDetailsProps): JSX.Element {
             <Header/>
             <div className='application-details left-5'>
                 <h2 className='topic-text'>{application?.trainingTopic}</h2>
-                {(role === Role.manager) && (application?.desiredManagerId === userId) && (status === 'Ждёт согласования руководителя') &&
+                {/*(role === Role.manager) && (application?.desiredManagerId === userId) && (status === 'Ждёт согласования руководителя') &&
                     <AcceptDeclineButton TrainingApplicationId={application.trainingApplicationId}/>
                 }
                 {(role === Role.admin) &&
                     <SubmitButton4 text={'Оформление'}/>
-                }
-                <p className='bold-text'>Статус:</p>
+                */}
                 <div className='flex border-2 rounded-xl items-center w-fit pr-4'>
                     <StatusIcon variant={status} className='mr-[8px]'/>
                     {status}
                 </div>
+                
                 <div className='flex w-full gap-[50px]'>
                     {application?.applicationUserName &&
                         <IconNameCombo name={application?.applicationUserName} action='Подал'/>}
-                    {afterManagerApprovalStatuses.includes(status) ?
-                        application?.desiredManagerName &&
-                        <IconNameCombo name={application?.desiredManagerName} action='Одобрил'/>
-                        : null}
+                    
+                    {application?.desiredManagerName &&
+                        <IconNameCombo name={application?.desiredManagerName} action='Согласует'/>}
                 </div>
 
                 <ModeSwitchButton contentMode={dataFlag} setContentMode={setDataFlag} leftPartText={'Исходная заявка'}
                                   rightPartText={'Утвержденные данные'}/>
                 {!dataFlag ?
-                    <div className='pending-application-details'>
-                        <TextValueBlock textValueProps={[
-                            ['Количество участников', application?.plannedParticipantsCount],
-                            ['ФИО участников', application?.plannedParticipantsNames],
-                            ['Департамент', application?.department],
-                            ['Отдел/команда', application?.team],
-                            ['Согласующий руководитель', application?.desiredManagerName]
-                        ]}/>
-                        <TextValueBlock textValueProps={[
-                            ['Формат', `${application ? (application?.isTrainingOnline ? 'Онлайн,' : 'Оффлайн,') : ''}
-                        ${application ? (application?.isCorporateTraining ? 'только для нашей компании' : 'не только для нашей компании') : ''}`],
-                            ['Желаемые даты', `${stringToDate(application?.desiredBegin)} - ${stringToDate(application?.desiredEnd)}`],
-                            ['Похожие курсы', application?.similarPrograms],
-                            ['Стоимость на одного', `${application?.estimatedCostPerParticipant} рублей`]
-                        ]}/>
-                        <TextValueBlock textValueProps={[
-                            ['Мотивация', application?.relevanceReason],
-                            ['Цели обучения', application?.trainingGoals],
-                            ['Приобретаемые навыки', application?.skillsToBeAcquired],
-                            ['Примечания', application?.applicationNotes]
-                        ]}/>
-                    </div>
-                    : <div className='pending-application-details'>
-                        <TextValueBlock textValueProps={[
-                            ['Учебный центр', course?.educationalCenter],
-                            ['Название курса', course?.courseName],
-                            ['Формат', `${course ? (course?.isTrainingOnline ? 'Онлайн,' : 'Оффлайн,') : ''} 
-                            ${course ? (course?.isCorporateTraining ? 'только для нашей компании' : 'не только для нашей компании') : ''}`],
-                            ['Даты', `${stringToDate(course?.begin)} - ${stringToDate(course?.end)}`],
-                            ['Стоимость на одного', course?.costPerParticipant]
-                        ]}/>
-                        <TextValueBlock textValueProps={[
-                            ['Количество участников', course?.participantsCount],
-                            ['ФИО участников', course?.participantsNames],
-                            ['Департамент', course?.department],
-                            ['Отдел/команда', course?.team]
-                        ]}/>
-                    </div> }
+                    <PendingApplicationDetails application={application} />
+                    : <ApprovedApplicationDetails course={course} application={application} /> }
                 {application && role && ((role===Role.admin) || userId === application?.applicationUserId || userId === application.desiredManagerId) &&
                 <CommentSendField trainingApplicationId={id} applicationUserId={application?.applicationUserId} userId={userId} role={roleEnum}/>}
 
--- external_training/ClientApp/src/types/application.tsx
+++ external_training/ClientApp/src/types/application.tsx
@@ -4,44 +4,51 @@ export type Application = {
     trainingApplicationId: number;
     trainingTopic: string;
     status: string;
-    "desiredManagerId": string;
-    desiredManagerName: string;
-    "applicationUserId": string,
-    "applicationUserName": string,
-    plannedParticipantsCount: number;
-    plannedParticipantsNames: string;
+    applicationUserId: string;
+    applicationUserName: string;
     department: string;
     team: string;
-    isTrainingOnline: boolean;
-    isCorporateTraining: boolean;
-    desiredBegin: string;
-    desiredEnd: string;
-    estimatedCostPerParticipant: number;
     similarPrograms: string;
     relevanceReason: string;
     trainingGoals: string;
     skillsToBeAcquired: string;
     applicationNotes: string;
+    createdAt: string;
     comments: CommentsType;
+    approvingManagers: ApprovingManager[];
+    participants: Participant[];
+    course: Course;
+}
+
+export type ApprovingManager = {
+    personId: string;
+    appointmentId: string;
+    postName: string;
+    orgUnitName: string;
+    lastName: string;
+    firstName: string;
+    middleName: string;
+}
+
+export type Participant = {
+    soloPersonId: string;
+    fullName: string;
+    lastName: string;
+    firstName: string;
+    middleName: string;
 }
 
 export type Course = {
-    trainingApplicationId: number,
-    trainingTopic: string,
-    status: string,
-    applicationUserId: string,
-    desiredManagerId: string,
-    desiredManagerName: string,
-    educationalCenter: string,
-    courseName: string,
-    participantsCount: number,
-    participantsNames: string,
-    department: string,
-    team: string,
-    isTrainingOnline: boolean,
-    isCorporateTraining: boolean,
-    begin: string,
-    end: string,
-    costPerParticipant: number,
-    comments: CommentsType;
+    courseId: number;
+    trainingApplicationId: number;
+    name: string;
+    isTrainingOnline: boolean;
+    isCorporateTraining: boolean;
+    category: string;
+    description: string;
+    trainingCenter: string;
+    costPerParticipant: number;
+    totalCost: number;
+    begin: string;
+    end: string;
 }
